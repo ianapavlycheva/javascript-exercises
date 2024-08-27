f78c494c41788a31393ba162aac8f79658a20a5f
Iana Pavlycheva <ianapavlycheva@gmail.com>
2024-08-27 15:15:13 +1000

    Complete  08-12 exercices

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const isPalindrome = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  return isPalindrome === isPalindrome.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = parseInt(num);
+  if (num < 0) {
+    return "OOPS";
+  } else if (num === 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else if (num === 2) {
+    return 1;
+  } else {
+    let a = 1,
+      b = 1,
+      temp;
+    for (let i = 3; i <= num; i++) {
+      temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
+  }
 };
 
 // Do not edit below this line
